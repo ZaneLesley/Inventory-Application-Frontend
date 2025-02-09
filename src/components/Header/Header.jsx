1ebaaef3b6795c698f30ff7f26ba801122fa7543
ZaneLesley <zanelesley@outlook.com>
2025-02-09 00:35:11 -0600

    feat(css): started css styling of the LandingPage.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,9 @@
+import './Header.css';
+
 import {Link} from 'react-router-dom';
 import {useEffect, useState} from "react";
+import Icon from '@mdi/react'
+import {mdiMenu} from '@mdi/js';
 
 function Header({tabs = []}) {
     const [storeName, setStoreName] = useState(null);
@@ -9,20 +13,23 @@ function Header({tabs = []}) {
         fetch('http://localhost:3000/api/stores', {mode: 'cors'})
             .then((response) => response.json())
             .then((data) => {
-                setStoreName(data[0].name)
+                setStoreName(data[0].name);
                 setStoreId(data[0].id);
             })
             .catch((error) => console.log('Fetch error:', error));
     }, []);
     return (<div className="header">
-        <div className="navbar">
-            img
-        </div>
-        <div className="store-name">
-            {storeName}
-        </div>
-        <div className="store-id">
-            {storeId}
+        <div className="container">
+            <div className="navbar">
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent"><Icon path={mdiMenu} size={1}/></button>
+            </div>
+            <div className="store-name">
+                {storeName}
+            </div>
+            <div className="store-id">
+                Store #{storeId}
+            </div>
         </div>
         <div className="tabs">
             {tabs.map((tab, index) => (<Link key={index} to={tab.url} className={"tab"}>
